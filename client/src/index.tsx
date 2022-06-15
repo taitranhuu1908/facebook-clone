a98f08cc26a6fd7ff312e20bd33f90349ac3dd89
taitranhuu1908 <emlacuaanh1908@gmail.com>
2022-06-15 14:08:06 +0700

    feat: config redux and react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
