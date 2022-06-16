c8905ed368d5af7e472a85db050b7481dc114b3f
tokyoHuyhieu <hieu.nguyenphanhuy@students.hueic.edu.vn>
2022-06-16 18:41:40 +0700

    feat: component create post

--- client/src/pages/test.tsx
+++ client/src/pages/test.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CreatePost from '../components/create-post';
 
 function Test() {
     return (
-        <div>Test</div>
-    )
+        <CreatePost name="Hiếu" avatar="https://scontent.fdad3-4.fna.fbcdn.net/v/t1.6435-9/186540483_2924150637822211_8758031143615848898_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=P2_ieTLYf9sAX_fAsmQ&_nc_ht=scontent.fdad3-4.fna&oh=00_AT_-B9DfSpDzfEp94aqc1_bIlBMIzo5Yg-ZKGwkqMjajtQ&oe=62D0EC21"/>
+    );
 }
 
 export default Test
